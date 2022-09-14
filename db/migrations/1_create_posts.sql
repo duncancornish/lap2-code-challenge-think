6e37bb4bc15dd4189cf83f678524665a6f47e2a3
Littlerou <izzymelvin4@gmail.com>
2022-09-14 15:48:55 +0100

    syntax error

--- db/migrations/1_create_posts.sql
+++ db/migrations/1_create_posts.sql
@@ -4,5 +4,5 @@ CREATE TABLE posts (
     id serial PRIMARY KEY,
     title varchar(100) NOT NULL,
     pseudonym varchar(100),
-    content varchar(255) NOT NULL,
+    content varchar(255) NOT NULL
 );
